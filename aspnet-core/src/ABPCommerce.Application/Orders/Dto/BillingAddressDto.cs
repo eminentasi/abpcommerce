a067ac2ff280c8a94ba7fce33929a86814e5efa8
ramiassi <ramiassi@gmail.com>
2021-12-22 17:08:19 +0300

    Move the BillingAddressDto & ShippingAddressDto to the right place

---
+++ aspnet-core/src/ABPCommerce.Application/Orders/Dto/BillingAddressDto.cs
@@ -0,0 +1,28 @@
+﻿using Abp.Application.Services.Dto;
+using ABPCommerce.OrderManagement.Billing;
+using System.ComponentModel.DataAnnotations;
+
+namespace ABPCommerce.Orders.Dto
+{
+    public class BillingAddressDto : EntityDto
+    {
+        [MaxLength(BillingAddress.NameMaxLength)]
+        public string FirstName { get; set; }
+        [MaxLength(BillingAddress.NameMaxLength)]
+        public string LastName { get; set; }
+        [MaxLength(500)]
+        public string CompanyName { get; set; }
+        [MaxLength(BillingAddress.NameMaxLength)]
+        public string Country { get; set; }
+        [MaxLength(BillingAddress.NameMaxLength)]
+        public string State { get; set; }
+        [MaxLength(BillingAddress.NameMaxLength)]
+        public string City { get; set; }
+        [MaxLength(BillingAddress.PostcodeMaxLength)]
+        public string Postcode { get; set; }
+        [MaxLength(BillingAddress.AddressMaxLength)]
+        public string Address { get; set; }
+        [MaxLength(BillingAddress.TaxIdMaxLength)]
+        public string TaxId { get; set; }
+    }
+}
